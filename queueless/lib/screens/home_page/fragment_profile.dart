e69875caed1114552ea00e773f6e4c2f6b8cceeb
Aditya <kathpalia.aditya11051999@gmail.com>
2020-06-21 01:12:25 +0530

    Profile Page updated

--- queueless/lib/screens/home_page/fragment_profile.dart
+++ queueless/lib/screens/home_page/fragment_profile.dart
@@ -12,231 +12,213 @@ class _ProfileFragmentState extends State<ProfileFragment> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body:
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16,40,16,0),
-            child: SingleChildScrollView(
-              child: Column(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16,40,16,0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Random',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0
+                      ),
+                    ),
+                    Text('emailid@email.com',
+                        style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.normal,
+                            fontSize: 14.0
+                        ),
+                      ),
+                    Text('View Profile',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14.0
+                      ),
+                    )
+                  ],
+                ),
+                CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('assets/images/propic.png'),
+                ),
+              ],
+
+            ),
+              SizedBox(height: 8),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              Row(
+                children: <Widget>[
+                  Image.asset('assets/images/order.png', width:24, height:24),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child:Text('Orders',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14.0
+                      ),
+                    ),
+
+                  ),
+                  Text('>',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0
+                    ),
+                  ),
+
+
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Image.asset('assets/images/heart.png', width:24, height:24),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child:Text('Help',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14.0
+                      ),
+                    ),
+
+                  ),
+                  Text('>',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0
+                    ),
+                  ),
+
+
+                ],
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              Row(
                 children: <Widget>[
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Random',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20.0
-                          ),
-                        ),
-                        Text('emailid@email.com',
-                            style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.normal,
-                                fontSize: 14.0
-                            ),
-                          ),
-                        Text('View Profile',
-                          style: TextStyle(
-                              color: Colors.red,
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14.0
-                          ),
-                        )
-                      ],
+                  Expanded(
+                    child:Text('About',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14.0
+                      ),
                     ),
 
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: <Widget>[
-                        Container(
-                          height: 100.0,
-                          width: 100.0,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15.0),
-                              image: DecorationImage(
-                                  image: AssetImage('assets/images/propic.png'),
-                                  fit: BoxFit.cover
-                              )
-                          ),
-                        ),
-                      ],
+                  ),
+                  Text('>',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0
+                    ),
+                  ),
+
+
+                ],
+              ),
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+              Row(
+                children: <Widget>[
+
+                  Expanded(
+                    child:Text('Send Feedback',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14.0
+                      ),
                     ),
 
-                  ],
-
-                ),
-                  SizedBox(height: 8),
-                  Divider(
-                    color: Colors.grey,
-                    thickness: 1,
                   ),
-                  Row(
-                    children: <Widget>[
-                      Image.asset('assets/images/order.png', width:24, height:24),
-                      SizedBox(width: 8),
-                      Expanded(
-                        child:Text('Orders',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14.0
-                          ),
-                        ),
-
-                      ),
-                      Text('>',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20.0
-                        ),
-                      ),
-
-
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Image.asset('assets/images/heart.png', width:24, height:24),
-                      SizedBox(width: 8),
-                      Expanded(
-                        child:Text('Help',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14.0
-                          ),
-                        ),
-
-                      ),
-                      Text('>',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20.0
-                        ),
-                      ),
-
-
-                    ],
-                  ),
-                  Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                  ),
-                  Row(
-                    children: <Widget>[
-
-                      Expanded(
-                        child:Text('About',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14.0
-                          ),
-                        ),
-
-                      ),
-                      Text('>',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20.0
-                        ),
-                      ),
-
-
-                    ],
-                  ),
-                  Divider(
-                    color: Colors.grey,
-                    thickness: 1,
-                  ),
-                  Row(
-                    children: <Widget>[
-
-                      Expanded(
-                        child:Text('Send Feedback',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14.0
-                          ),
-                        ),
-
-                      ),
-                      Text('>',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20.0
-                        ),
-                      ),
-
-
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-
-                      Expanded(
-                        child:Text('Rate us',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14.0
-                          ),
-                        ),
-
-                      ),
-                      Text('>',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20.0
-                        ),
-                      ),
-
-
-                    ],
-                  ),
-                  Row(
-                    children: <Widget>[
-
-                      Expanded(
-                        child:Text('Log Out',
-                          style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.normal,
-                              fontSize: 14.0
-                          ),
-                        ),
-
-                      ),
-                      Text('>',
-                        style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.normal,
-                            fontSize: 20.0
-                        ),
-                      ),
-
-
-                    ],
+                  Text('>',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0
+                    ),
                   ),
 
 
-                    ],
+                ],
+              ),
+              Row(
+                children: <Widget>[
+
+                  Expanded(
+                    child:Text('Rate us',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14.0
+                      ),
+                    ),
+
                   ),
-            ),
+                  Text('>',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0
+                    ),
+                  ),
+
+
+                ],
+              ),
+              Row(
+                children: <Widget>[
+
+                  Expanded(
+                    child:Text('Log Out',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14.0
+                      ),
+                    ),
+
+                  ),
+                  Text('>',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.normal,
+                        fontSize: 20.0
+                    ),
+                  ),
+
+
+                ],
+              ),
+            ],
           ),
-
-              );
-
+        ),
+      ),
+    );
   }
 }
